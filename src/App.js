770603f8a8c4c22d1d38ccb810b31e900f80b63f
Robert Aeschlimann <robert.aeschlimann@post.ch>
2021-08-13 09:36:03 +0200

    No comment

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Example from Udemy - by aroweb
+          ...troubleshootin...
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
